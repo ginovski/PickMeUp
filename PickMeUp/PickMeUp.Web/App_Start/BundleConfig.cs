dd664c3637f7190d8d4741d0ab5e45a87e022073
Alexandar Ginovski <sashko.ginovski@gmail.com>
2014-11-08 12:56:14 +0200

    Changed bootstrap theme

--- PickMeUp/PickMeUp.Web/App_Start/BundleConfig.cs
+++ PickMeUp/PickMeUp.Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace PickMeUp.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.flatly.css",
                       "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
